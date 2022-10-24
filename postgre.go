8ceb4931545d447698f7c0f1c5142eff39d271a9
yuzevan <111624645+yuzevan@users.noreply.github.com>
2022-10-24 09:06:21 +0700

    8

---
+++ postgre.go
@@ -0,0 +1,26 @@
+package connection
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/jackc/pgx/v5"
+)
+
+var Conn *pgx.Conn
+
+func DatabaseConnect() {
+	var err error
+
+	
+	databaseUrl := "postgres://postgres:123@localhost:5432/personal_web_b41"
+
+	Conn, err = pgx.Connect(context.Background(), databaseUrl)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Println("Success connect to database")
+}
